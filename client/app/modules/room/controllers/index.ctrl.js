1a2d3c863e1e7971926e446959a57fe0c7041279
Anh Tuấn Hoàng <anhtuanhnh@gmail.com>
2016-12-31 13:32:35 +0700

    a

---
+++ client/app/modules/room/controllers/index.ctrl.js
@@ -0,0 +1,55 @@
+'use strict';
+angular
+  .module('com.module.index')
+  .controller('index', function () {
+
+  })
+  .controller('slideCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    $scope.slides = [
+      {
+        image: 'images/slide/1.jpg',
+        id: 0
+      },
+      {
+        image: 'images/slide/2.jpg',
+        id: 1
+      },
+      {
+        image: 'images/slide/5.jpg',
+        id: 2
+      }
+    ];
+
+})
+  .controller('reviewCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    $scope.slides = [
+      {
+        image: 'images/man/man1.jpg',
+        id: 0
+      },
+      {
+        image: 'images/man/man2.jpg',
+        id: 1
+      },
+      {
+        image: 'images/man/man3.jpg',
+        id: 2
+      }
+    ];
+
+  })
+
+  .controller('ss2info', function () {
+
+  })
+
+  .controller('ss3slides', function () {
+
+  });
+
